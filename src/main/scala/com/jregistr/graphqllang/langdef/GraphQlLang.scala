f7137ee73542b3ba72af2f57aa493cca82211ebc
Jeff Registre <jeffreyregistre@gmail.com>
2018-11-29 18:28:44 -0500

    Initial Commit
    Basic grammar and graphql file type are defined.

---
+++ src/main/scala/com/jregistr/graphqllang/langdef/GraphQlLang.scala
@@ -0,0 +1,35 @@
+package com.jregistr.graphqllang.langdef
+
+import com.intellij.lang.Language
+import com.intellij.openapi.fileTypes.{FileTypeConsumer, FileTypeFactory, LanguageFileType}
+import com.intellij.psi.tree.IElementType
+import com.jregistr.graphqllang.util.GraphQlLangIcons
+import javax.swing.Icon
+
+object GraphQlLang extends Language("GraphQL")
+
+object GraphQlLangFileType extends LanguageFileType(GraphQlLang) {
+
+  override val getName: String = "GraphQL File"
+
+  override val getDescription: String = "GraphQL schema definition or query file."
+
+  override val getDefaultExtension: String = "graphql"
+
+  override val getIcon: Icon = GraphQlLangIcons.FileIcon
+
+}
+
+class GraphQlLangFileTypeFactory extends FileTypeFactory {
+
+  override def createFileTypes(consumer: FileTypeConsumer): Unit = consumer.consume(GraphQlLangFileType)
+
+}
+
+class GraphQlLangTokenType(debugName: String) extends IElementType(debugName, GraphQlLang) {
+
+  override def toString: String = s"GraphQlTokenType.${super.toString}"
+
+}
+
+class GraphQlLangElementType(debugName: String) extends IElementType(debugName, GraphQlLang)
---
+++ src/main/scala/com/jregistr/graphqllang/util/GraphQlLangIcons.scala
@@ -0,0 +1,10 @@
+package com.jregistr.graphqllang.util
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object GraphQlLangIcons {
+
+  val FileIcon: Icon = IconLoader.getIcon("/img/graphql_16x16.png")
+
+}
