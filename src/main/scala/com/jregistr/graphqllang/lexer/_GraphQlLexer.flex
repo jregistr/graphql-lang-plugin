7d76ef5ef8fea65a29ec28f933eee6a4660ab222
Jeff Registre <jeffreyregistre@gmail.com>
2018-12-13 00:51:42 -0500

    Rewritting grammar and focusing on matching closer to spec.

--- src/main/scala/com/jregistr/graphqllang/lexer/_GraphQlLexer.flex
+++ src/main/scala/com/jregistr/graphqllang/lexer/_GraphQlLexer.flex
@@ -25,7 +25,7 @@ import static com.jregistr.graphqllang.psi.GraphQlTokenTypes.*;
 EOL=\R
 WHITE_SPACE=\s+
 
-DIRECTIVE_IDENTIFIER=@([_A-Za-z][_0-9A-Za-z]*)
+DIRECTIVE_IDENTIFIER=@(_?[A-Za-z][_0-9A-Za-z]*)
 VARIABLE_IDENTIFIER=\$([_A-Za-z][_0-9A-Za-z]*)
 IDENTIFIER=[_A-Za-z][_0-9A-Za-z]*
 STRING_LITERAL=\".*\"
@@ -37,6 +37,7 @@ SINGLE_LINE_COMMENT=#.*
 <YYINITIAL> {
   {WHITE_SPACE}               { return WHITE_SPACE; }
 
+  "NOT_YET_IMPL"              { return NOT_IMPL; }
   "type"                      { return TYPE; }
   "input"                     { return INPUT; }
   "enum"                      { return ENUM; }
@@ -62,8 +63,20 @@ SINGLE_LINE_COMMENT=#.*
   "="                         { return EQUALS; }
   "|"                         { return PIPE; }
   "!"                         { return REQUIRED; }
+  "&"                         { return AMPERSAND; }
   "..."                       { return FRAGMENT_DOTS; }
-  "on"                        { return FRAGMENT_ON; }
+  "on"                        { return ON; }
+  "UnionTypeDef"              { return UNIONTYPEDEF; }
+  "ScalarTypeDef"             { return SCALARTYPEDEF; }
+  "InputObjectTypeDef"        { return INPUTOBJECTTYPEDEF; }
+  "ExtendObjectTypeDef"       { return EXTENDOBJECTTYPEDEF; }
+  "ExtendScalarDef"           { return EXTENDSCALARDEF; }
+  "ExtendInterfaceDef"        { return EXTENDINTERFACEDEF; }
+  "ExtendUnionDef"            { return EXTENDUNIONDEF; }
+  "ExtendEnumDef"             { return EXTENDENUMDEF; }
+  "ExtendInputObjectDef"      { return EXTENDINPUTOBJECTDEF; }
+  "OperationDef"              { return OPERATIONDEF; }
+  "FragmentDef"               { return FRAGMENTDEF; }
 
   {DIRECTIVE_IDENTIFIER}      { return DIRECTIVE_IDENTIFIER; }
   {VARIABLE_IDENTIFIER}       { return VARIABLE_IDENTIFIER; }
---
+++ src/main/scala/com/jregistr/graphqllang/psi/DirectiveLocation.scala
@@ -0,0 +1,87 @@
+package com.jregistr.graphqllang.psi
+
+sealed trait DirectiveLocation {
+  val schemaValue: String
+  require(schemaValue.toUpperCase == schemaValue)
+}
+
+sealed trait TypeSystemDirectiveLocation extends DirectiveLocation
+
+case object Schema extends TypeSystemDirectiveLocation {
+  override val schemaValue: String = "SCHEMA"
+}
+
+case object Scalar extends TypeSystemDirectiveLocation {
+  override val schemaValue: String = "SCALAR"
+}
+
+case object Object extends TypeSystemDirectiveLocation {
+  override val schemaValue: String = "OBJECT"
+}
+
+case object FieldDefinition extends TypeSystemDirectiveLocation {
+  override val schemaValue: String = "FIELD_DEFINITION"
+}
+
+case object ArgumentDefinition extends TypeSystemDirectiveLocation {
+  override val schemaValue: String = "ARGUMENT_DEFINITION"
+}
+
+case object Interface extends TypeSystemDirectiveLocation {
+  override val schemaValue: String = "INTERFACE"
+}
+
+case object Union extends TypeSystemDirectiveLocation {
+  override val schemaValue: String = "UNION"
+}
+
+case object Enum extends TypeSystemDirectiveLocation {
+  override val schemaValue: String = "ENUM"
+}
+
+case object EnumValue extends TypeSystemDirectiveLocation {
+  override val schemaValue: String = "ENUM_VALUE"
+}
+
+case object InputObject extends TypeSystemDirectiveLocation {
+  override val schemaValue: String = "INPUT_OBJECT"
+}
+
+case object InputFieldDefinition extends TypeSystemDirectiveLocation {
+  override val schemaValue: String = "INPUT_FIELD_DEFINITION"
+}
+
+/*Executable directive locations*/
+sealed trait ExecutableDirectiveLocation extends DirectiveLocation
+
+case object Query extends ExecutableDirectiveLocation {
+  override val schemaValue: String = "QUERY"
+}
+
+case object Mutation extends ExecutableDirectiveLocation {
+  override val schemaValue: String = "MUTATION"
+}
+
+case object Subscription extends ExecutableDirectiveLocation {
+  override val schemaValue: String = "SUBSCRIPTION"
+}
+
+case object Field extends ExecutableDirectiveLocation {
+  override val schemaValue: String = "FIELD"
+}
+
+case object FragmentDefinition extends ExecutableDirectiveLocation {
+  override val schemaValue: String = "FRAGMENT_DEFINITION"
+}
+
+case object FragmentSpread extends ExecutableDirectiveLocation {
+  override val schemaValue: String = "FRAGMENT_SPREAD"
+}
+
+case object InlineFragment extends ExecutableDirectiveLocation {
+  override val schemaValue: String = "INLINE_FRAGMENT"
+}
+
+case object VariableDefinition extends ExecutableDirectiveLocation {
+  override val schemaValue: String = "VARIABLE_DEFINITION"
+}
